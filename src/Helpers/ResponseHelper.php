1fc6a22130fab22d13bfa3764bf1354b1375ad9e
Mehdi Hashemi <m.hashemi@dornica.net>
2023-02-20 14:52:19 +0330

    Fix encryption

--- src/Helpers/ResponseHelper.php
+++ src/Helpers/ResponseHelper.php
@@ -2,16 +2,10 @@
 
 namespace Sirj3x\Websocket\Helpers;
 
-use App\Libraries\EncryptionLibrary;
-
 trait ResponseHelper
 {
     public function success($data = []): array
     {
-        if (config('websocket.io_encryption')) {
-            $data = EncryptionLibrary::encrypt(json_encode($data), config('websocket.io_encryption_secret_key'));
-        }
-
         return [
             'status' => 200,
             'data' => $data
@@ -20,17 +14,11 @@ trait ResponseHelper
 
     public function error($messages, $statusCode): array
     {
-        $data = [
-            'message' => [$messages]
-        ];
-
-        if (config('websocket.io_encryption')) {
-            $data = EncryptionLibrary::encrypt(json_encode($data), config('websocket.io_encryption_secret_key'));
-        }
-
         return [
             'status' => $statusCode,
-            'data' => $data
+            'data' => [
+                'message' => [$messages]
+            ]
         ];
     }
 }
